9b23c6666993d0361cab34334855a89038ea838e
Ihot Tretiak <TretyakIgor2603@gmail.com>
2019-11-25 12:34:15 +0200

    Added HOST variable

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ require('colors');
 
 const app = require('./modules/app');
 
-const APP_PORT = process.env.SERVER_PORT | 3000;
+const APP_PORT = parseInt(process.env.SERVER_PORT || 3000, 10);
+const APP_HOST = process.env.SERVER_HOST || 'http://localhost';
 
 app.listen(APP_PORT, () => {
-    console.log(`App is running on http://localhost:${APP_PORT}`.yellow.bold);
+    console.log(`App is running on ${APP_HOST}:${APP_PORT}`.yellow.bold);
 });
 
 // TODO
