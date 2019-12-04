3c3dfcb983137e3c9c750e9e460db03ab48cb3bd
Ihor Tretiak <TretyakIgor2603@gmail.com>
2019-12-04 11:27:15 +0200

    Small fix

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 require('colors');
 
 const app = require('./modules/app');
+const connectToMysqlDb = require('./configs/mysql-db.js');
 
 const APP_PORT = parseInt(process.env.SERVER_PORT || 8080, 10);
-const APP_HOST = process.env.SERVER_HOST || '0.0.0.0';
+const APP_HOST = process.env.SERVER_HOST || '127.0.0.1';
+
+const connectionMysql = connectToMysqlDb();
 
 app.listen(APP_PORT, () => {
     console.log(`App is running on ${APP_HOST}:${APP_PORT}`.yellow.bold);
 });
-
-// TODO
-// Add database to Docker
